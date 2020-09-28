fa785cd6e556419deb71c46d515aba07c8f10fd0
EricThrift <EricThrift@users.noreply.github.com>
2020-09-28 11:36:30 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zotero-meta-analysis-toolkit", 
+    version="0.0.1",
+    author="Eric Thrift",
+    author_email="dried.fish.matters@umanitoba.ca",
+    description="Command-line tool to support basic meta-analysis using a library managed in Zotero",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DriedFishMatters/zotero-meta-analysis-toolkit,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    requires=[
+        "pyzotero",
+        "click",
+        "unicodecsv",
+        "pandas"
+    ]
+)
